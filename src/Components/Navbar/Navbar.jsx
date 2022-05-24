6b0b61c4b09ebaebe1b17e3556025d35b5abe1e9
viniciusbortolotti <vbortolotti@outlook.com>
2022-05-24 14:02:57 -0300

    coisa.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../../Assets/Images/logo.png'
 import './navbar.css'
 
 function Navbar() {
+
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    localStorage.clear();
+    navigate('/')
+  }
+
   return (
     <header>
-        <div className="header">
-            <Link to="/"><img src={logo} alt="logo" class="logo" /></Link>
-            <nav>
-                <ul>
-                    <li><Link to="/vagas">Vagas</Link></li>
-                    <li><Link to="/notificacoes">Notificações</Link></li>
-                    <li><Link to="/perfil">Meu Perfil</Link></li>
-                </ul>
-            </nav>
-        </div>
+<div class="header">
+        <Link to="/"><img src={logo} alt="logo" class="logo" /></Link>
+        <nav>
+            <ul>
+                <li><Link to="/vagas"><strong>Vagas</strong></Link></li>
+                <li><Link to="/notificacoes"><strong>Notificações</strong></Link></li>
+                <li><Link to="/perfil"><strong>Meu Perfil</strong></Link></li>
+                <li><Link to="/" onClick={(e) => logOut()}><strong>Sair</strong></Link></li>
+            </ul>
+        </nav>
+    </div>
     </header>
   )
 }

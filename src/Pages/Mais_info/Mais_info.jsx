6bd5e880010de00bf727c4ce1886d53801a0a1d2
Vinicius <80995705+viniciuS-Simizu-reg1sem2021@users.noreply.github.com>
2022-05-27 19:47:38 -0300

    correcting mais_info page

--- src/Pages/Mais_info/Mais_info.jsx
+++ src/Pages/Mais_info/Mais_info.jsx
@@ -22,51 +22,44 @@ function Mais_info() {
     }, [])
 
     return(
-        <>
-        <div class="bloco2-mais-info">
-            <div class="campos-mais-info">
-                <strong>Campos</strong>
-            </div>
-            <div class="busque-mais-info">
-                <strong>Busque por Bicos</strong>
-            </div>
-        </div>
-        <div class="fundo-mais-info">
-<div class="vaga-mais-info">
-    <div class="bloco3-mais-info">
-        <div class="descricao-mais-info">
-            <strong>ghgfhf</strong>
-            <p>dfsfd}</p>
-        </div>
-            <Link to="/candidatar" class="botao-mais-info"><strong>Candidatar-se</strong></Link>
-    </div>
-    <div class="bloco4-mais-info">
-        <div class="contratante-mais-info">
-            <img src={img13}/>
-            <div class="nome-mais-info">
-                <strong>Nome Sobrenome</strong>
-            </div>
-            <div class="classificacao-mais-info">
-                classificacao
-            </div>
-            <div class="localizacao-mais-info">
-                <div class="local_img-mais-info">
+
+        <div id={'mais_info'}>
+
+            <div id={'mais_info_header'} className={'w-100'}>
+
+                <div style={{ backgroundColor: '#B4A8B7' }} className={'text-light'}>
+                    BICOS DISPONÍVEIS
                 </div>
-                <div class="local-mais-info">
-                <strong>Colinas de Indaiatuba<br />
-                Indaiatuba-SP</strong>
+
+                <div style={{ backgroundColor: '#070C3B' }}></div>
+
+            </div>
+
+            <Container className={'mt-4'}>
+
+                <div className={'px-5 py-4'}>
+
+                    <div id={'left_part'} className={'p-3'}>
+                        <h2>Cortar a grama</h2>
+                        <p>mais info</p>
+                        <button className={'mx-auto'}>Candidatar-se</button>
+                    </div>
+
+                    <div>
+
+                    </div>
+
                 </div>
-            </div>
-            <div class="preco-mais-info">
-                <strong>40,00</strong>
-            </div>
+
+                <div>
+                    f
+                </div>
+
+            </Container>
+
         </div>
-    </div>
-</div>
-<Link to="/Vagas"><a class="saber_menos-mais-info">-</a></Link>
-</div>
-</>
-)
+
+    )
 }
 
 export default Mais_info;

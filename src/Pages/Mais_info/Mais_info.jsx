964177153ebf6117d348313c08cdd9611aced980
Vinicius <80995705+viniciuS-Simizu-reg1sem2021@users.noreply.github.com>
2022-05-27 22:53:52 -0300

    mais_info page 60%

--- src/Pages/Mais_info/Mais_info.jsx
+++ src/Pages/Mais_info/Mais_info.jsx
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from "react";
 import {Link, useParams} from "react-router-dom"
 import "./mais_info.scss";
 import img13 from '../../Assets/Images/13.png';
+import adriano from '../../Assets/Images/Adrian_de_cria.png';
 import myaxios from "../../Services/axios";
 import {Container} from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import StarRatingComponent from "react-star-rating-component";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStar, faLocationDot} from "@fortawesome/free-solid-svg-icons";
 
 
 function Mais_info() {
@@ -42,17 +47,50 @@ function Mais_info() {
                     <div id={'left_part'} className={'p-3'}>
                         <h2>Cortar a grama</h2>
                         <p>mais info</p>
-                        <button className={'mx-auto'}>Candidatar-se</button>
+                        <div className={'d-flex'}>
+                            <button className={'text-light'}>Candidatar-se</button>
+                        </div>
                     </div>
 
                     <div>
+                        <div className={'h-100 w-100'}>
 
+                            <div className={''}>
+
+                                <div className={'d-flex justify-content-center align-items-center'}>
+                                    <Image src={adriano} width={100} className={'border border-1'} roundedCircle />
+                                </div>
+
+                                <div className={'px-3 d-flex align-items-center'}>
+                                    <div className={'d-flex flex-column'}>
+
+                                        <h6>Conde Dracula</h6>
+                                        <section className={'d-flex align-items-center'}>
+                                            <StarRatingComponent starColor={'#FAC113'} editing={false} value={3} emptyStarColor={'transparent'} renderStarIcon={() => <FontAwesomeIcon className={'px-1'} icon={faStar} />} />
+                                            <span>3</span>
+                                        </section>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={'d-flex align-items-center'}>
+
+                                <FontAwesomeIcon icon={faLocationDot} />
+
+                            </div>
+
+                            <div>
+f
+                            </div>
+
+                        </div>
                     </div>
 
                 </div>
 
                 <div>
-                    f
+                    fffff
                 </div>
 
             </Container>

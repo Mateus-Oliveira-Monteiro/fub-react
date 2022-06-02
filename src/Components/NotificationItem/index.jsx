98b1ac82f41b748db6242a5715d4862979799821
Vinicius <80995705+viniciuS-Simizu-reg1sem2021@users.noreply.github.com>
2022-06-01 22:13:07 -0300

    notification time by minutes/hours/days

--- src/Components/NotificationItem/index.jsx
+++ src/Components/NotificationItem/index.jsx
@@ -1,11 +1,67 @@
 import Image from "react-bootstrap/Image";
 import './notificationItem.scss';
+import {useEffect, useReducer} from "react";
 
 export const NotificationItem = props => {
 
-    const { image, content, created_at } = props.notification
+    const TimeReducer = (state, action) => {
+
+        switch (action.type) {
+
+            case 'INICIALIZA':
+                return action.state;
+
+            default:
+                return state;
+
+        }
+
+    }
+
+    const INITIAL_STATE = {
+        time: 0,
+        format: 'min',
+    };
+
+    const [timeState, dispatch] = useReducer(TimeReducer, INITIAL_STATE);
+
+    const { image, content, created_at } = props.notification;
 
     const minutes = Math.floor((Date.now() - new Date(created_at).getTime()) / 1000 / 60)
+
+    useEffect(() => {
+
+        if (minutes >= 60 * 24) {
+            dispatch({
+                type: 'INICIALIZA',
+                state: {
+                    time: Math.floor(minutes / 60 / 24),
+                    format: 'dia',
+                },
+            });
+        } else if(minutes >= 60) {
+            dispatch({
+                type: 'INICIALIZA',
+                state: {
+                    time: Math.floor(minutes / 60),
+                    format: 'hrs',
+                },
+            });
+        } else {
+            dispatch({
+                type: 'INICIALIZA',
+                state: {
+                    time: minutes,
+                    format: 'min',
+                },
+            });
+        }
+
+    }, [])
+
+    useEffect(() => {
+        console.log(timeState)
+    }, [timeState])
 
     return (
         <div className={'py-1 row d-flex align-items-center w-100 notification-item'}>
@@ -20,7 +76,7 @@ export const NotificationItem = props => {
             </span>
 
             <span className={'col-2'}>
-                { minutes } min
+                { timeState.time } { timeState.format }
             </span>
         </div>
     )
